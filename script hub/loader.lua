898ac6d8224db56cff043d7b70169822519f1c6a
xyz177 <112547284+xyz177@users.noreply.github.com>
2023-02-16 17:57:47 +0800

    Update loader.lua

--- script hub/loader.lua
+++ script hub/loader.lua
@@ -1,1 +1,9 @@
-
+if game.PlaceId ~= 0 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xyz177/roblox-scripts/main/script%20hub/universal.lua", true))()
+end
+if game.PlaceId == 8571687919 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xyz177/roblox-scripts/main/script%20hub/unofficial.lua", true))()
+end 
+if game.PlaceId == 12017032683 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xyz177/roblox-scripts/main/script%20hub/096.lua", true))()
+end
