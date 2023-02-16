125a2a1c510be5d08e6d8192394241691407aa10
xyz177 <112547284+xyz177@users.noreply.github.com>
2023-02-16 18:18:37 +0800

    Update unofficial.lua

--- script hub/unofficial.lua
+++ script hub/unofficial.lua
@@ -1,22 +1,26 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("bing chilling | UNOFFICIAL", "GrapeTheme")
 
---Tabs
+--Main
 
-local MainTab = Window:NewTab("Main")
-local MainTab1 = MainTab:NewSection("MainScripts")
-Section:NewKeybind("KeybindText", "KeybindInfo", Enum.KeyCode.F, function()
+local Main = Window:NewTab("Main")
+local MainSection = Main:NewSection("Main")
 
 --Buttons
-
-MainTab1:NewButton("Auto6Slap", "yes", function()
+MainSection:NewButton("Auto6Slap", "yes", function()
     while wait() do
         game:GetService("ReplicatedStorage").Remotes.SlapOnSix:FireServer()
     end
 end)
 
-MainTab1:NewButton("AutoOfficial", "auto official", function()
+MainSection:NewButton("AutoOfficial", "official", function()
     while wait() do
 		game:GetService("ReplicatedStorage").Remotes.CallOfficial:FireServer()
 	end
 end)
+
+
+MainSection:NewKeybind("UI Key", "key", Enum.KeyCode.F, function()
+	Library:ToggleUI()
+end)
+
