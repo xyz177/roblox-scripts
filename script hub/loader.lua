132b24858f4caa3ce00571dc104f9cdfdb450493
xyz177 <112547284+xyz177@users.noreply.github.com>
2023-02-18 12:13:08 +0800

    Delete script hub directory

--- script hub/loader.lua
+++
@@ -1,6 +0,0 @@
-if game.PlaceId == 8571687919 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/xyz177/roblox-scripts/main/script%20hub/unofficial.lua", true))()
-end 
-if game.PlaceId == 12017032683 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/xyz177/roblox-scripts/main/script%20hub/096.lua", true))()
-end
