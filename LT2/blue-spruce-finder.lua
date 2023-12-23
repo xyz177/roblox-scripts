fd573076787af2b7f5692667c6db67ff4afb139f
xyz177 <112547284+xyz177@users.noreply.github.com>
2023-12-23 17:23:30 +0800

    Create blue-spruce-finder.lua

---
+++ LT2/blue-spruce-finder.lua
@@ -0,0 +1,17 @@
+getgenv().Config = {
+
+    TreesToFind = {"BlueSpruce"},
+
+    MinimumUnits = 1,
+
+    ESPTree = false,
+
+    AutoClaimTree = false,
+
+    CopyGameInvite = true,
+
+    WebHook = nil
+
+}
+
+loadstring(game:HttpGet"https://raw.githubusercontent.com/Aurora-2004/AuroraPublic/main/SpookFinder")("Aurora On Top")
